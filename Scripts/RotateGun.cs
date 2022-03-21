b30ac51b69d2f08aeabd043f4fd83963ce252d43
NatanTrosmanGMIT <61838651+NatanTrosmanGMIT@users.noreply.github.com>
2022-03-21 12:42:56 +0000

    Add files via upload
    All scripts so far

---
+++ Scripts/RotateGun.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+// Rotates the gun towrds the grapple point and snaps it back when finished
+public class RotateGun : MonoBehaviour {
+
+    public GrapplingGun grappling;
+
+    private Quaternion desiredRotation;
+    private float rotationSpeed = 5f;
+
+    void Update() {
+        if (!grappling.IsGrappling()) {
+            desiredRotation = transform.parent.rotation;
+        }
+        else {
+            desiredRotation = Quaternion.LookRotation(grappling.GetGrapplePoint() - transform.position);
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, Time.deltaTime * rotationSpeed);
+    }
+
+}
